41d70fe6c8c5cc5c7beba3534afb86374408daa3
BALAND Jérome <baland.jerome@gmail.com>
2024-03-09 20:29:42 +0100

    changement affichage avec ancien calcul

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inputs = document.querySelectorAll("input");
 let number1 = "";
 let number2 = "";
+let total;
 let numbersDisplay = "";
 let indicator = "";
 
@@ -19,17 +20,18 @@ function div(a, b) {
 
 inputs.forEach((input) => {
   input.addEventListener("click", (e) => {
-    console.log(e.target.value);
     if (e.target.value.match(/[\d_.]/)) {
       number1 +=
         e.target.value == "." && number1.length === 0
           ? "0" + e.target.value
           : e.target.value;
-
-      display.innerText = `${number1}`;
+      display.innerHTML = `
+      <p>${total ? "" : number2} ${indicator}</p>
+      <p>${number1}</p>
+      `;
     } else if (e.target.value.match(/[+-/|x]/)) {
       number1 = parseFloat(number1);
-      display.innerText = e.target.value;
+
       if (indicator.length > 0 && number1) {
         if (indicator == "+") {
           number2 = add(number2, number1);
@@ -43,30 +45,38 @@ inputs.forEach((input) => {
       } else {
         number2 = number1 ? number1 : number2;
       }
+      total = "";
+      display.innerHTML = `
+      <p>${number2 ? number2 : total} ${e.target.value}</p>
+      <p></p>
+      `;
       number1 = "";
       indicator = e.target.value;
     } else if (e.target.value == "=") {
       number1 = parseFloat(number1);
       if (indicator.length > 0 && number1) {
         if (indicator == "+") {
-          number2 = add(number2, number1);
+          total = add(number2, number1);
         } else if (indicator == "-") {
-          number2 = sub(number2, number1);
+          total = sub(number2, number1);
         } else if (indicator == "x") {
-          number2 = mult(number2, number1);
+          total = mult(number2, number1);
         } else if (indicator == "/") {
-          number2 = div(number2, number1);
+          total = div(number2, number1);
         }
+        display.innerHTML = `
+        <p>${number2} ${indicator} ${number1}</p>
+        <p>= ${total}</p>
+        `;
+        number2 = total;
+        indicator = "";
       }
-      indicator = "";
       number1 = "";
-      display.innerText = number2;
     } else {
-      display.innerText = "";
+      display.innerHTML = "";
       number1 = "";
       number2 = "";
       indicator = "";
     }
-    console.log(number1, indicator, number2);
   });
 });
